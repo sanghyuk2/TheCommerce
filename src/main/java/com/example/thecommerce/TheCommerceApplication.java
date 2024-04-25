ad380c0e60802bb227353c92ccd9c5576b5f8359
sanghyuk2 <givemesomefoodplz@gmail.com>
2024-04-25 13:48:05 +0900

    [Setting] Spring 환경 설정

---
+++ src/main/java/com/example/thecommerce/TheCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.thecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheCommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thecommerce/TheCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

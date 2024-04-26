0e7ff5f98ade019491c6a1b36d214ddb8058a96d
sanghyuk2 <givemesomefoodplz@gmail.com>
2024-04-26 12:04:59 +0900

    feat: 회원 목록 조회 기능 구현

--- src/main/java/com/example/thecommerce/controller/UserController.java
+++ src/main/java/com/example/thecommerce/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.example.thecommerce.controller;
 
 import com.example.thecommerce.dto.UserReqDto;
+import com.example.thecommerce.entity.User;
 import com.example.thecommerce.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -23,5 +23,13 @@ public class UserController {
         userService.join(userReqDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body("회원 가입 성공");
+    }
+
+    @GetMapping("/list")
+    public List<User> getUserList(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int pageSize,
+            @RequestParam(defaultValue = "joinDate") String sort) {
+        return userService.getUserList(page, pageSize, sort);
     }
 }

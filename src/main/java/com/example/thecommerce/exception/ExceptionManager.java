309c82889243402d4630704851548a97149e4d62
sanghyuk2 <givemesomefoodplz@gmail.com>
2024-04-27 15:18:55 +0900

    feat: 예외 처리 추가

--- src/main/java/com/example/thecommerce/exception/ExceptionManager.java
+++ src/main/java/com/example/thecommerce/exception/ExceptionManager.java
@@ -1,8 +1,13 @@
 package com.example.thecommerce.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class ExceptionManager {
@@ -10,5 +15,17 @@ public class ExceptionManager {
     public ResponseEntity<?> appExceptionHandler(AppException e) {
         return ResponseEntity.status(e.getErrorCode().getHttpStatus())
                 .body(e.getErrorCode().name() + " " + e.getMessage());
+    }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> validationExceptionHandler(ConstraintViolationException e) {
+
+        String message = "";
+
+        for (ConstraintViolation<?> constraintViolation : e.getConstraintViolations()) {
+            message += " " + constraintViolation.getMessage();
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("USER_BAD_REQUEST " + message);
     }
 }
